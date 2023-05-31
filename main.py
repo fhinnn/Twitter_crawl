ba9945b2cd0c3a03c6c54b1423ceb33adf3a4a9c
fhinnn <deltras12388@gmail.com>
2023-05-31 06:26:29 +0000

    feat: update crawler

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import snscrape.modules.twitter as sntwitter
 import csv
 
-def crawl_twitter_hashtag(hashtag, num_tweets, start_date, end_date, output_file):
-    query = f'{hashtag} since:{start_date} until:{end_date}'
+def crawl_twitter_hashtag(hashtags, num_tweets, start_date, end_date, output_file):
     tweets = []
 
-    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-        tweets.append(tweet.rawContent)
-        if len(tweets) >= num_tweets:
-            break
+    for hashtag in hashtags:
+        query = f'{hashtag} since:{start_date} until:{end_date}'
+        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+            if any(tag.lower() in tweet.content.lower() for tag in hashtags):
+                tweets.append(tweet.content)
+                if len(tweets) >= num_tweets:
+                    break
 
     with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
@@ -16,4 +18,25 @@ def crawl_twitter_hashtag(hashtag, num_tweets, start_date, end_date, output_file
         for tweet in tweets:
             writer.writerow([tweet])
 
-crawl_twitter_hashtag("pemilu", 10000, '2021-01-01', '2023-12-31', 'output.csv')
+hashtags = ["#Pemilu",
+"#Pemilu2024",
+"#PemiluLegislatif",
+"#PemiluPresiden",
+"#Demokrasi",
+"#Politik",
+"#PartaiPolitik",
+"#SuaraRakyat",
+"#PerubahanPolitik",
+"#CalonLegislatif",
+"#CalonPresiden",
+"#Kampanye",
+"#DebatPublik",
+"#DebatPilpres",
+"#PemilihCerdas",
+"#HakPilih",
+"#DemokrasiIndonesia",
+"#PemiluSehat",
+"#PemiluDamai",
+"#TerkiniPemilu"]
+
+crawl_twitter_hashtag(hashtags, 100, '2021-01-01', '2023-12-31', 'hasil/tweets.csv')
